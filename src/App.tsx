16bc8f827ee56fa521124803b2b7f15a46004c0d
Mathieu.B <mathieu.bst@gmail.com>
2024-10-07 21:23:37 +0200

    react-dom-router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import './App.css'
+
+
+function App() {
+
+  return (
+    <h1>React: react-router-dom</h1>
+  );
+}
+
+export default App
